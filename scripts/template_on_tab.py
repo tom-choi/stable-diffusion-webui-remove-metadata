da349a47db2b78ca63412750f6f57bc85c20d106
tom-choi <cvb233299@gmail.com>
2024-06-23 11:43:47 +0800

    remove template.py

--- scripts/template_on_tab.py
+++ scripts/template_on_tab.py
@@ -34,10 +34,6 @@ def remove_metadata(image_path):
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as ui_component:
         with gr.Row():
-            checkbox = gr.Checkbox(
-                True,
-                label="Show image"
-            )
             btn = gr.Button(
                 "Remove Metadata"
             ).style(
@@ -49,7 +45,7 @@ def on_ui_tabs():
             )
             gallery = gr.Gallery(
                 label="Dummy Image",
-                show_label=False,
+                show_label=True,
             )
 
         btn.click(
@@ -58,37 +54,9 @@ def on_ui_tabs():
             outputs = [gallery],
         )
 
-        return [(ui_component, "Extension Example", "extension_example_tab")]
+        return [(ui_component, "", "extension_example_tab")]
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
-
-
-# from PIL import Image
-# from PIL.ExifTags import TAGS
-# import os
-
-# def remove_metadata(image_path):
-#     """
-#     Removes the metadata from an image file and saves a new file with a prefix.
-    
-#     Args:
-#         image_path (str): The path to the image file.
-#     """
-#     image = Image.open(image_path)
-    
-#     # Get the directory and filename
-#     directory, filename = os.path.split(image_path)
-#     name, extension = os.path.splitext(filename)
-    
-#     # Create the new filename with a prefix
-#     new_filename = f"no_metadata_{name}{extension}"
-#     new_path = os.path.join(directory, new_filename)
-    
-#     # Remove the EXIF data and save the new file
-#     image.save(new_path, optimize=True, quality=100)
-
-#     print(f"Metadata removed from {image_path}")
-#     print(f"New file saved as: {new_path}")
 
 # # Usage example
 # remove_metadata("G:\github\\novelai\\novelai-webui-10-9update\outputs\extras-images\\00082.png")
